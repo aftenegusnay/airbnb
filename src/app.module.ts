3ebd29c7f3ca572ae609037c43cf3f1f9ddddafa
aftnegusnay <aftnegusnay@uce.edu.ec>
2025-03-31 23:30:23 -0500

    🌐 i18n(eslint): añadir configuración ESLint para soporte de Prettier   - Agregar archivo .eslintrc.json con configuración básica   - Incluir "eslint:recommended" y "plugin:prettier/recommended" como extensiones   - Configurar regla "prettier/prettier" para errores de fin de línea
    🔧 chore(prettier): actualizar configuración de Prettier
      - Añadir "endOfLine": "lf" a .prettierrc para consistencia de línea final
    
    📦 build(dependencies): actualizar dependencias de proyecto
      - Agregar @nestjs/typeorm, pg y typeorm a las dependencias
      - Actualizar @nestjs/mapped-types a la versión ^2.1.0
    
    ✨ feat(database): integrar TypeORM con configuración PostgreSQL
      - Configurar TypeOrmModule en app.module.ts para conexión con PostgreSQL
      - Añadir entidad User a TypeOrmModule para mapeo de datos
    
    🔧 chore(users): comentar funciones no utilizadas en el módulo de usuarios
      - Desactivar funciones de creación y actualización en users.controller.ts y users.service.ts
      - Comentar importaciones relacionadas con DTOs no utilizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity'; // Asegúrate de importar la entidad correcta
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // O cambia a 'mysql', 'mariadb', etc.
+      host: 'bkwljfugeifqquhtliiu.supabase.co',
+      port: 5432, // Puerto por defecto de PostgreSQL
+      username: 'aftenegusnay',
+      password: 'airbnbcloneelian',
+      database: 'airbnb',
+      entities: [User], // Incluye aquí todas tus entidades
+      synchronize: true, // Solo en desarrollo, en producción usa migrations
+      logging: true, // Para ver las consultas en consola
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
+// src/users/users.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controller/users.controller';
 
 @Module({
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
-  exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
