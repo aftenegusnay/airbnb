d480c4c8619a864ea59d2d28e9f6f7a84dfa95f3
Elian Condor <econdor@ec.krugercorp.com>
2025-05-06 10:48:25 -0500

    update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './services/users.service';
-import { UsersController } from './controller/users.controller';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
